34e7d15540c0d22d51e363b716c6773ad572ee31
GraphicDThanh <nguyendiemthanhdn92@gmail.com>
2024-10-05 21:36:51 +0700

    update path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/eplantshopping",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
